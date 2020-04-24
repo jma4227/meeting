50b5f834654762c9cc2c09ac87c4a39526bb8203
james.ma <j.ma4227@gmail.com>
2020-04-24 16:25:03 -0500

    added api.py to meeting, modified meeting.js and meeting.py

--- meeting/config/docs.py
+++ meeting/config/docs.py
@@ -2,10 +2,10 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/meeting"
-# docs_base_url = "https://[org_name].github.io/meeting"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/jma4226/meeting"
+docs_base_url = "https://jma4227.github.io/meeting"
+headline = "App that does everything"
+sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
 	context.brand_html = "Meeting"
