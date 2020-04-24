8124ebe9db182fcc82e2ee0b2e0dc4aa811b32e2
james.ma <j.ma4227@gmail.com>
2020-04-24 16:27:32 -0500

    added api.py to meeting, modified meeting.js and meeting.py

--- meeting/config/docs.py
+++ meeting/config/docs.py
@@ -1,11 +1,21 @@
-"""
-Configuration for docs
-"""
-
-source_link = "https://github.com/jma4226/meeting"
-docs_base_url = "https://jma4227.github.io/meeting"
-headline = "App that does everything"
-sub_heading = "Yes, you got that right the first time, everything"
-
-def get_context(context):
-	context.brand_html = "Meeting"
+# """
+# Configuration for docs
+# """
+#
+# source_link = "https://github.com/jma4226/meeting"
+# docs_base_url = "https://jma4227.github.io/meeting"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+#
+#
+# def get_context(context):
+# 	# optional settings
+#
+# 	# context.brand_html = 'Brand info on the top left'
+# 	# context.favicon = 'path to favicon'
+# 	#
+# 	# context.top_bar_items = [
+# 	#   {"label": "About", "url": context.docs_base_url + "/about"},
+# 	# ]
+#
+# 	pass
