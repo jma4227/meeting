a8929d268e59ccb6713922d1cef14f9da94fc697
james.ma <j.ma4227@gmail.com>
2020-04-24 13:14:16 -0500

    -modified asset.js

--- meeting/meeting/doctype/meeting/meeting.py
+++ meeting/meeting/doctype/meeting/meeting.py
@@ -11,10 +11,17 @@ from frappe.model.document import Document
 
 class Meeting(Document):
 	def validate(self):
-		"""Set missing names"""
+		"""Set missing names and warn if duplicate"""
+		found = []
 		for attendee in self.attendees:
 			if not attendee.full_name:
 				attendee.full_name = get_full_name(attendee.attendee)
+				
+			if attendee.attendee in found:
+				frappe.throw(_("Attendee {0} entered twice").format(attendee.attendee))
+				
+				found.append(attendee.attendee)
+			
 
 @frappe.whitelist()
 def get_full_name(attendee):
