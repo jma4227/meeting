8a59874a39887bf0fbf5d47c6d9b4a48632ec334
james.ma <j.ma4227@gmail.com>
2020-04-24 14:11:53 -0500

    added api.py to meeting, modified meeting.js and meeting.py

---
+++ meeting/config/tools.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from frappe import _
+
+
+def get_data():
+	return [
+		{
+			"label": _("Tools"),
+			"icon": "octicon octicon-briefcase",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Meeting",
+					"label": _("Meeting"),
+					"description": _("."),
+					"onboard": 1,
+					},
+				]
+			}
+		]
