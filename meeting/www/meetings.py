cb8f928ca7054b98a3f8ff1302a977ebf28f5729
james.ma <j.ma4227@gmail.com>
2020-04-25 14:46:02 -0500

    modified meetings.html, meetings.py and added meetings.css

--- meeting/www/meetings.py
+++ meeting/www/meetings.py
@@ -4,10 +4,11 @@ import frappe
 def get_context(context):
 	context.planned_meetings = get_meetings("Planned")
 	
+	#  show only 20 past meetings
 	context.past_meetings = get_meetings("Completed", limit_page_length = 20)
 
 
 def get_meetings(status, **kwargs):
 	return frappe.get_all("Meeting", fields = ["name", "title", "date", "from_time", "to_time"],
-						  filters = {"status": "Completed", "show_in_website": 1},
+						  filters = {"status": status, "show_in_website": 1},
 						  order_by = "date desc", **kwargs)
