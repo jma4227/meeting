20d92e5fdb8210a9877ebf9d5d33817a14d20c13
james.ma <j.ma4227@gmail.com>
2020-04-25 13:25:58 -0500

    modified meeting.py, test_meeting.py api.py, hooks.py

--- meeting/meeting/doctype/meeting/meeting.py
+++ meeting/meeting/doctype/meeting/meeting.py
@@ -6,13 +6,20 @@ from __future__ import unicode_literals
 
 import frappe
 from frappe import _
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
 # test
 
-class Meeting(Document):
+class Meeting(WebsiteGenerator):
+	website = frappe._dict(
+		template = "templates/generators/meeting.html",
+	
+	)
+	
 	def validate(self):
+		self.page_name = self.name.lower()
+		
 		self.validate_attendees()
 	
 	def on_update(self):
